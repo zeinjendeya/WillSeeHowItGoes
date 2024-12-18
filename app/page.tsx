4fd38321c98abb34ad37dd0ae7266acd4851630c
Zein Jendeya <z.jendeya@gmail.com>
2024-12-18 10:23:52 +0200

    used the fonts added to fonts.ts in page, layout, and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { lusitana } from './ui/fonts';
 // import {shape} from 'app/ui/home.module.css';
 
 export default function Page() {
@@ -13,7 +14,7 @@ export default function Page() {
         
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
 
-          <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
+          <p className={`${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal `}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -28,7 +29,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          
+        <AcmeLogo />
         </div>
       </div>
     </main>
