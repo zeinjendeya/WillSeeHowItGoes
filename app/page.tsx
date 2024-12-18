6b7fa2174c78151d0718ad881d52bbdabd18257d
Zein Jendeya <z.jendeya@gmail.com>
2024-12-18 10:35:33 +0200

    added mobile image

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,13 @@ export default function Page() {
         height={760}
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"/>
+
+        <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="md:hidden sm:block"
+        alt="Screenshots of the dashboard project showing mobile version" />
         </div>
       </div>
     </main>
