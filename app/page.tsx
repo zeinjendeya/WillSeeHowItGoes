a3080b2cfa1b0ef8980d2b69534d1187b0bb1678
Zein Jendeya <z.jendeya@gmail.com>
2024-12-18 10:38:34 +0200

    modified the class of mobile image

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Page() {
         src="/hero-mobile.png"
         width={560}
         height={620}
-        className="md:hidden sm:block"
+        className="block md:hidden"
         alt="Screenshots of the dashboard project showing mobile version" />
         </div>
       </div>
