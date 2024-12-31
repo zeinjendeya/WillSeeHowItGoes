9cd730668a44d648eb6f777b0a61c951ebc71f91
Zein Jendeya <z.jendeya@gmail.com>
2024-12-31 15:13:53 +0200

    fixed main layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,28 @@ import { Metadata } from "next";
 import Footer from "./ui/up/footer/footer";
 import NavBarLG from "./ui/up/header/nav-bar-lg";
 import NavBarMS from "./ui/up/header/nav-bar-ms";
+import './ui/global.css'
 
 export const metadata: Metadata = {
   title: "Unified Portal",
   description: "The tryout for the Unified Portal.",
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="flex flex-col">
-      <NavBarLG />
-      <NavBarMS />
-      <div>{children}</div>
-      <div>{/* comment section - section will be divided up later */}</div>
-      <Footer />
-    </div>
+    <html lang="en">
+      <body>
+        <NavBarLG />
+        <NavBarMS />
+        <div>{children}</div>
+        <div>{/* comment section - section will be divided up later */}</div>
+        <Footer />
+      </body>
+    </html>
   );
 }
