54a51d1cc551f71c0e7e144986a1e0dc8e73ac75
Zein Jendeya <z.jendeya@gmail.com>
2025-01-02 13:45:08 +0200

    initial classes and breadcrumb ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Breadcrumb from "./ui/up/elements/breadcrumb";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 
 export default function Page(){
-  const breadcrumb = [{name: "Home", href: "/"}, {name: "Page", href: "/page"}];
+  const breadcrumbs = {
+    page: 'main',
+    links: [{name: <a className="w-full" href="#">الرئيسية</a>, href: "/"}, {name: <ChevronLeftIcon strokeWidth={2.5} className="h-3 w-3 block" />, href: '#'}, {name: <a className="w-full" href="#">الرئيسية</a>, href: "/page"}]
+  };
+  
   return (
       <main>
-          {/* <Breadcrumb {...breadcrumb} /> */}
-        
+        <div className="section-container flex flex-col relative">
+          <Breadcrumb {...breadcrumbs} />
+        </div>
       </main>
   );
 }
--- app/ui/up/elements/breadcrumb.tsx
+++ app/ui/up/elements/breadcrumb.tsx
@@ -1,16 +1,23 @@
+import { JSX } from "react";
+
 interface List {
-  name: string;
+  name: JSX.Element;
   href: string;
 }
 
-export default function Breadcrumb(links: List[]) {
+interface BreadcrumbProps {
+  page: string;
+  links: List[];
+}
+
+export default function Breadcrumb({ page, links }: BreadcrumbProps) {
   return (
-    <div className="bg-[#d7e5de] section-container flex flex-col items-center justify-center px-6 py-2">
+    <div className=" rounded-b-3xl bg-[#d7e5de] label12 flex items-center justify-end px-4 py-2 lg:px-6 lg:py-2">
       <nav aria-label="breadcrumb">
-        <ol>
+        <ol className="flex flex-row-reverse items-center gap-2.5 lg:gap-1.5">
           {links.map((link) => (
-            <li>
-                <a href={link.href}>{link.name}</a>
+            <li key={link.href} className="gap-1.5">
+              {link.name}
             </li>
           ))}
         </ol>
